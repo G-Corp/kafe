39d58161845eb569502aa0db94bac077a9555c0b
glejeune <gregoire.lejeune@free.fr>
2016-04-06 17:29:03 +0200

    Update erlang.mk
    Prepare for common tests

---
+++ test/dist_SUITE.erl
@@ -0,0 +1,98 @@
+-module(dist_SUITE).
+-include_lib("common_test/include/ct.hrl").
+
+-export([
+         all/0
+         , init_per_suite/1
+         , end_per_suite/1
+        ]).
+
+-export([
+         my_test_case/0
+         , my_test_case/1
+        ]).
+
+%%--------------------------------------------------------------------
+%% Function: init_per_suite(Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the suite.
+%%
+%% Description: Initialization before the suite.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+	Config.
+
+%%--------------------------------------------------------------------
+%% Function: end_per_suite(Config0) -> term() | {save_config,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Description: Cleanup after the suite.
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+	ok.
+
+%%--------------------------------------------------------------------
+%% Function: all() -> GroupsAndTestCases | {skip,Reason}
+%%
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%%   Name of a test case group.
+%% TestCase = atom()
+%%   Name of a test case.
+%% Reason = term()
+%%   The reason for skipping all groups and test cases.
+%%
+%% Description: Returns the list of groups and test cases that
+%%              are to be executed.
+%%--------------------------------------------------------------------
+all() -> 
+	[my_test_case].
+
+
+%%--------------------------------------------------------------------
+%% TEST CASES
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Function: TestCase() -> Info
+%%
+%% Info = [tuple()]
+%%   List of key/value pairs.
+%%
+%% Description: Test case info function - returns list of tuples to set
+%%              properties for the test case.
+%%
+%% Note: This function is only meant to be used to return a list of
+%% values, not perform any other operations.
+%%--------------------------------------------------------------------
+my_test_case() -> 
+	[].
+
+%%--------------------------------------------------------------------
+%% Function: TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%% Comment = term()
+%%   A comment about the test case that will be printed in the html log.
+%%
+%% Description: Test case function. (The name of it must be specified in
+%%              the all/0 list or in a test case group for the test case
+%%              to be executed).
+%%--------------------------------------------------------------------
+my_test_case(_Config) -> 
+	ok.
+
