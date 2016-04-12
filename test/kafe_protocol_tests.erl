d5c1fbc1075f8cf5eba625dd062a0c78b18c7923
glejeune <gregoire.lejeune@free.fr>
2016-04-12 12:17:26 +0200

    Update tests

--- test/kafe_protocol_tests.erl
+++ test/kafe_protocol_tests.erl
@@ -7,6 +7,7 @@ kafe_protocol_test_() ->
   {setup, fun setup/0, fun teardown/1,
    [
     ?_test(t_protocol_request()),
+    ?_test(t_protocol_request_force_api_version()),
     ?_test(t_protocol_encode_string()),
     ?_test(t_protocol_encode_bytes()),
     ?_test(t_protocol_encode_array())
@@ -20,15 +21,28 @@ teardown(_) ->
   ok.
 
 t_protocol_request() ->
-  ?assertEqual(#{packet => <<0,0,0,14,0,1,0,0,0,0,0,0,0,4,116,101,115,116>>,
+  ?assertEqual(#{api_version => 0,
+                 packet => <<0,0,0,14,0,1,0,0,0,0,0,0,0,4,116,101,115,116>>,
                  state => ?REQ_STATE2(1, 0)},
                kafe_protocol:request(1, <<>>, ?REQ_STATE2(0, 0))),
-  ?assertEqual(#{packet => <<0,0,0,14,0,1,0,1,0,0,0,0,0,4,116,101,115,116>>,
+  ?assertEqual(#{api_version => 1,
+                 packet => <<0,0,0,14,0,1,0,1,0,0,0,0,0,4,116,101,115,116>>,
                  state => ?REQ_STATE2(1, 1)},
                kafe_protocol:request(1, <<>>, ?REQ_STATE2(0, 1))),
-  ?assertEqual(#{packet => <<0,0,0,14,0,1,0,2,0,0,0,0,0,4,116,101,115,116>>,
+  ?assertEqual(#{api_version => 2,
+                 packet => <<0,0,0,14,0,1,0,2,0,0,0,0,0,4,116,101,115,116>>,
                  state => ?REQ_STATE2(1, 2)},
                kafe_protocol:request(1, <<>>, ?REQ_STATE2(0, 2))).
+
+t_protocol_request_force_api_version() ->
+  ?assertEqual(#{api_version => 0,
+                 packet => <<0,0,0,14,0,1,0,0,0,0,0,0,0,4,116,101,115,116>>,
+                 state => ?REQ_STATE2(1, 1)},
+               kafe_protocol:request(1, <<>>, ?REQ_STATE2(0, 1), 0)),
+  ?assertEqual(#{api_version => 0,
+                 packet => <<0,0,0,14,0,1,0,0,0,0,0,0,0,4,116,101,115,116>>,
+                 state => ?REQ_STATE2(1, 2)},
+               kafe_protocol:request(1, <<>>, ?REQ_STATE2(0, 2), 0)).
 
 t_protocol_encode_string() ->
   ?assertEqual(kafe_protocol:encode_string(undefined),
