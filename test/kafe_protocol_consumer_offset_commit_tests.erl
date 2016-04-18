ff3aa3f1603806bdf151f596d0cefff1ac5cd409
glejeune <gregoire.lejeune@free.fr>
2016-04-18 08:33:41 +0200

    Update tests

--- test/kafe_protocol_consumer_offset_commit_tests.erl
+++ test/kafe_protocol_consumer_offset_commit_tests.erl
@@ -19,30 +19,35 @@ teardown(_) ->
 
 t_request() ->
   ?assertEqual(
+     #{api_version => 0,
+       packet =>
+       <<0,0,0,44,0,8,0,0,0,0,0,0,0,4,116,101,115,116,0,13,67,111,
+         110,115,117,109,101,114,71,114,111,117,112,0,0,0,1,0,5,
+         116,111,112,105,99,0,0,0,0>>,
+       state => ?REQ_STATE2(1, 0)},
      kafe_protocol_consumer_offset_commit:request_v0(
-       <<"ConsumerGroup">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE(0)),
-     #{packet => <<0,0,0,44,0,8,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,
-                   110,115,117,109,101,114,71,114,111,117,112,0,0,0,1,0,5,
-                   116,111,112,105,99,0,0,0,0>>,
-       state => ?REQ_STATE(1)}),
+       <<"ConsumerGroup">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE2(0, 0))),
   ?assertEqual(
-     kafe_protocol_consumer_offset_commit:request_v1(
-       <<"ConsumerGroup">>, 1, <<"ConsumerId">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE(0)),
-     #{packet => <<0,0,0,60,0,8,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,117,109,
+     #{api_version => 1,
+       packet => <<0,0,0,60,0,8,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,117,109,
                    101,114,71,114,111,117,112,0,0,0,1,0,10,67,111,110,115,117,109,101,114,73,
                    100,0,0,0,1,0,5,116,111,112,105,99,0,0,0,0>>,
-       state => ?REQ_STATE(1)}),
+       state => ?REQ_STATE2(1, 1)},
+     kafe_protocol_consumer_offset_commit:request_v1(
+       <<"ConsumerGroup">>, 1, <<"ConsumerId">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE2(0, 1))),
   ?assertEqual(
-     kafe_protocol_consumer_offset_commit:request_v2(
-       <<"ConsumerGroup">>, 1, <<"ConsumerId">>, 3, [{<<"topic">>, [0,1,2]}], ?REQ_STATE(0)),
-     #{packet => <<0,0,0,68,0,8,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,117,109,
+     #{api_version => 2,
+       packet => <<0,0,0,68,0,8,0,2,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,117,109,
                    101,114,71,114,111,117,112,0,0,0,1,0,10,67,111,110,115,117,109,101,114,73,
                    100,0,0,0,0,0,0,0,3,0,0,0,1,0,5,116,111,112,105,99,0,0,0,0>>,
-       state => ?REQ_STATE(1)}).
+       state => ?REQ_STATE2(1, 2)},
+     kafe_protocol_consumer_offset_commit:request_v2(
+       <<"ConsumerGroup">>, 1, <<"ConsumerId">>, 3, [{<<"topic">>, [0,1,2]}], ?REQ_STATE2(0, 2))).
 
 t_response() ->
-  ?assertEqual(kafe_protocol_consumer_offset_commit:response(
-                 <<0,0,0,1,0,5,116,111,112,105,99,0,0,0,1,0,0,0,0,0,0>>),
-               {ok,[#{name => <<"topic">>,
-                      partitions => [#{error_code => none, partition => 0}]}]}).
+  ?assertEqual(
+     {ok,[#{name => <<"topic">>,
+            partitions => [#{error_code => none, partition => 0}]}]},
+     kafe_protocol_consumer_offset_commit:response(
+       <<0,0,0,1,0,5,116,111,112,105,99,0,0,0,1,0,0,0,0,0,0>>, 0)).
 
--- test/kafe_protocol_consumer_offset_fetch_tests.erl
+++ test/kafe_protocol_consumer_offset_fetch_tests.erl
@@ -18,37 +18,33 @@ teardown(_) ->
   ok.
 
 t_request() ->
-  ?assertEqual(kafe_protocol_consumer_offset_fetch:request(
-                 <<"ConsumerGroup">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE(0)),
-               #{packet => <<0,0,0,56,0,9,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,
-                             117,109,101,114,71,114,111,117,112,0,0,0,1,0,5,116,111,112,105,
-                             99,0,0,0,3,0,0,0,0,0,0,0,1,0,0,0,2>>,
-                 state => ?REQ_STATE(1)}).
+  ?assertEqual(
+     #{api_version => 1,
+       packet => <<0,0,0,56,0,9,0,1,0,0,0,0,0,4,116,101,115,116,0,13,67,111,110,115,
+                   117,109,101,114,71,114,111,117,112,0,0,0,1,0,5,116,111,112,105,
+                   99,0,0,0,3,0,0,0,0,0,0,0,1,0,0,0,2>>,
+       state => ?REQ_STATE2(1, 1)},
+     kafe_protocol_consumer_offset_fetch:request(
+       <<"ConsumerGroup">>, [{<<"topic">>, [0,1,2]}], ?REQ_STATE2(0, 1))).
 
 t_response() ->
-  ?assertEqual(kafe_protocol_consumer_offset_fetch:response(
-                 <<0,0,0,1,0,5,116,111,112,105,99,0,0,0,3,0,0,0,0,255,255,255,
-                   255,255,255,255,255,0,0,0,3,0,0,0,1,255,255,255,255,255,255,
-                   255,255,0,0,0,3,0,0,0,2,255,255,255,255,255,255,255,255,0,0,
-                   0,3>>),
-               {ok,[#{name => <<"topic">>,
-                      partitions_offset => [#{error_code => unknown_topic_or_partition,
-                                              metadata => <<>>,
-                                              offset => -1,
-                                              partition =>
-                                              2},
-                                            #{error_code => unknown_topic_or_partition,
-                                              metadata => <<>>,
-                                              offset => -1,
-                                              partition
-                                              =>
-                                              1},
-                                            #{error_code => unknown_topic_or_partition,
-                                              metadata => <<>>,
-                                              offset
-                                              =>
-                                              -1,
-                                              partition
-                                              =>
-                                              0}]}]}).
+  ?assertEqual(
+     {ok,[#{name => <<"topic">>,
+            partitions_offset => [#{error_code => unknown_topic_or_partition,
+                                    metadata => <<>>,
+                                    offset => -1,
+                                    partition => 2},
+                                  #{error_code => unknown_topic_or_partition,
+                                    metadata => <<>>,
+                                    offset => -1,
+                                    partition => 1},
+                                  #{error_code => unknown_topic_or_partition,
+                                    metadata => <<>>,
+                                    offset => -1,
+                                    partition => 0}]}]},
+     kafe_protocol_consumer_offset_fetch:response(
+       <<0,0,0,1,0,5,116,111,112,105,99,0,0,0,3,0,0,0,0,255,255,255,
+         255,255,255,255,255,0,0,0,3,0,0,0,1,255,255,255,255,255,255,
+         255,255,0,0,0,3,0,0,0,2,255,255,255,255,255,255,255,255,0,0,
+         0,3>>, 0)).
 
