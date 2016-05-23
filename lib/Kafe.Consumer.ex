7717b8f2e508e7b03a93928d6c56bd5d55082e06
glejeune <gregoire.lejeune@free.fr>
2016-05-23 14:23:50 +0200

    Update Elixir libs

--- lib/Kafe.Consumer.ex
+++ lib/Kafe.Consumer.ex
@@ -15,6 +15,21 @@ defmodule Kafe.Consumer do
 	def unquote(:"commit")(arg1) do
 		:erlang.apply(:"kafe_consumer", :"commit", [arg1])
 	end
+	def unquote(:"commit")(arg1, arg2) do
+		:erlang.apply(:"kafe_consumer", :"commit", [arg1, arg2])
+	end
+	def unquote(:"remove_commits")(arg1) do
+		:erlang.apply(:"kafe_consumer", :"remove_commits", [arg1])
+	end
+	def unquote(:"remove_commit")(arg1) do
+		:erlang.apply(:"kafe_consumer", :"remove_commit", [arg1])
+	end
+	def unquote(:"pending_commits")(arg1) do
+		:erlang.apply(:"kafe_consumer", :"pending_commits", [arg1])
+	end
+	def unquote(:"pending_commits")(arg1, arg2) do
+		:erlang.apply(:"kafe_consumer", :"pending_commits", [arg1, arg2])
+	end
 	def unquote(:"member_id")(arg1, arg2) do
 		:erlang.apply(:"kafe_consumer", :"member_id", [arg1, arg2])
 	end
@@ -32,6 +47,9 @@ defmodule Kafe.Consumer do
 	end
 	def unquote(:"topics")(arg1) do
 		:erlang.apply(:"kafe_consumer", :"topics", [arg1])
+	end
+	def unquote(:"server_pid")(arg1) do
+		:erlang.apply(:"kafe_consumer", :"server_pid", [arg1])
 	end
 	def unquote(:"start_link")(arg1, arg2) do
 		:erlang.apply(:"kafe_consumer", :"start_link", [arg1, arg2])
