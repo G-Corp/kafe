7fff8c82da24202bb0b2e071cb8bbe81fdfeb952
glejeune <gregoire.lejeune@free.fr>
2016-06-01 17:41:10 +0200

    Add generated mix.exs

--- Makefile
+++ Makefile
@@ -13,3 +13,6 @@ release: compile tests
 	@$(REBAR) elixir generate_mix
 	@$(REBAR) elixir generate_lib
 
+distclean:
+	@rm -rf _build rebar.lock mix.lock
+
--- mix.exs
+++ mix.exs
@@ -2,25 +2,31 @@ defmodule Kafe.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :kafe,
-     version: "1.3.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :kafe,
+      version: "1.3.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
   def application do
-    [applications: [:syntax_tools,:compiler,:poolgirl,:goldrush,:lager], mod: {:kafe_app, []}]
+    [
+       applications: [:kernel, :stdlib, :syntax_tools, :compiler, :poolgirl, :goldrush, :lager],
+       env: [],
+       mod: {:kafe_app, []}
+    ]
   end
 
   defp deps do
-    [ 
-      {:lager, ~r/.*/, git: "https://github.com/basho/lager.git", tag: "3.2.0"},  
-      {:bucs, ~r/.*/, git: "https://github.com/botsunit/bucs.git", tag: "0.0.1"},  
-      {:doteki, ~r/.*/, git: "https://github.com/botsunit/doteki.git", tag: "0.0.1"},  
-      {:poolgirl, ~r/.*/, git: "https://github.com/botsunit/poolgirl.git", tag: "0.0.1"},  
-      {:bristow, ~r/.*/, git: "https://github.com/botsunit/bristow.git", tag: "0.0.1"},
+    [
+      {:lager, "~> 3.2.0"},
+      {:bucs, git: "https://github.com/botsunit/bucs.git", tag: "0.0.1"},
+      {:doteki, git: "https://github.com/botsunit/doteki.git", tag: "0.0.1"},
+      {:poolgirl, git: "https://github.com/botsunit/poolgirl.git", tag: "0.0.1"},
+      {:bristow, git: "https://github.com/botsunit/bristow.git", tag: "0.0.1"}    
     ]
   end
 end
