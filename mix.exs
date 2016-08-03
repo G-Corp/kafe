c12087f0e388ee097812daf914919ff75a5ef0c6
glejeune <gregoire.lejeune@free.fr>
2016-08-03 12:59:27 +0200

    Prepare 1.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kafe.Mixfile do
   def project do
     [
       app: :kafe,
-      version: "1.5.2",
+      version: "1.6.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
