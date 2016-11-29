dbb7712b81d3e73ef667b5e4fde8daa33ee50878
glejeune <gregoire.lejeune@free.fr>
2016-11-29 11:13:48 +0100

    Bump to 2.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kafe.Mixfile do
   def project do
     [
       app: :kafe,
-      version: "2.1.2",
+      version: "2.1.3",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
